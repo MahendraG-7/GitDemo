318386db17d022a133da0f26779afbf78ad3097f
MahendraGothankar <mahendrag1@snapwork.com>
2026-03-09 16:28:43 +0530

    controller added

---
+++ src/main/java/com/example/GitDemo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.GitDemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping
+    public String message(){
+        return "git demo !!!!";
+    }
+
+
+
+}
